b3593f27d61d7190fe55df3fa3b36c07f0d7e9e6
Michaël Gallego <mic.gallego@gmail.com>
2013-03-20 10:47:41 +0100

    Add module dependencies (for easier debugging)

--- Module.php
+++ Module.php
@@ -64,4 +64,17 @@ class Module implements
             array('--waitTime=', 'Wait time (in seconds) for which the call will wait for a job to arrive in the queue before returning')
         );
     }
+    
+    /**
+     * This ModuleManager feature was introduced in ZF 2.1 to check if all the dependencies needed by a module
+     * were correctly loaded. However, as we want to keep backward-compatibility with ZF 2.0, please DO NOT
+     * explicitely implement Zend\ModuleManager\Feature\DependencyIndicatorInterface. Just write this method and
+     * the module manager will automatically call it
+     * 
+     * @return array
+     */
+    public function getModuleDependencies()
+    {
+        return array('SlmQueue');
+    }
 }
