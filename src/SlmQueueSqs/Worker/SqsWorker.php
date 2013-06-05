9dec4701a28e11a4df73e239dedb4e78f7b87e8b
Michaël Gallego <mic.gallego@gmail.com>
2013-06-05 09:46:03 +0200

    Add batch

--- src/SlmQueueSqs/Worker/SqsWorker.php
+++ src/SlmQueueSqs/Worker/SqsWorker.php
@@ -4,8 +4,10 @@ namespace SlmQueueSqs\Worker;
 
 use Exception;
 use SlmQueue\Job\JobInterface;
+use SlmQueue\Queue\QueueAwareInterface;
 use SlmQueue\Queue\QueueInterface;
 use SlmQueue\Worker\AbstractWorker;
+use SlmQueueSqs\Queue\SqsQueue;
 use SlmQueueSqs\Queue\SqsQueueInterface;
 
 /**
@@ -13,6 +15,60 @@ use SlmQueueSqs\Queue\SqsQueueInterface;
  */
 class SqsWorker extends AbstractWorker
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function processQueue($queueName, array $options = array())
+    {
+        // If user want to pop only one job, delegate this to the parent method
+        if (!isset($options['max_jobs']) || $options['max_jobs'] == 1) {
+            return parent::processQueue($queueName, $options);
+        }
+
+        /** @var $queue QueueInterface */
+        $queue = $this->queuePluginManager->get($queueName);
+        $count = 0;
+
+        // If we're here, this is batch pop, so we must have a SqsQueue
+        if (!$queue instanceof SqsQueueInterface) {
+            return $count;
+        }
+
+        while (true) {
+            // Check for external stop condition
+            if ($this->isStopped()) {
+                break;
+            }
+
+            $jobs = $queue->batchPop($options);
+
+            foreach ($jobs as $job) {
+                // The queue may return null, for instance if a timeout was set
+                if (!$job instanceof JobInterface) {
+                    continue;
+                }
+
+                // The job might want to get the queue injected
+                if ($job instanceof QueueAwareInterface) {
+                    $job->setQueue($queue);
+                }
+
+                $this->processJob($job, $queue);
+                $count++;
+
+                // Check for internal stop condition
+                if (
+                    $count === $this->options->getMaxRuns()
+                    || memory_get_usage() > $this->options->getMaxMemory()
+                ) {
+                    break;
+                }
+            }
+        }
+
+        return $count;
+    }
+
     /**
      * {@inheritDoc}
      */
