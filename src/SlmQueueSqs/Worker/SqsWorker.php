ba6720a78720e8d66370b2d461d5dc8064399903
Michaël Gallego <mic.gallego@gmail.com>
2014-01-19 17:35:21 +0100

    Worker catch any exception
    In SQS jobs are automatically reinserted if something bad happen, for
    further processing. Not catching exception makes the worker stop

--- src/SlmQueueSqs/Worker/SqsWorker.php
+++ src/SlmQueueSqs/Worker/SqsWorker.php
@@ -4,10 +4,8 @@ namespace SlmQueueSqs\Worker;
 
 use Exception;
 use SlmQueue\Job\JobInterface;
-use SlmQueue\Queue\QueueAwareInterface;
 use SlmQueue\Queue\QueueInterface;
 use SlmQueue\Worker\AbstractWorker;
-use SlmQueueSqs\Queue\SqsQueue;
 use SlmQueueSqs\Queue\SqsQueueInterface;
 
 /**
@@ -26,8 +24,12 @@ class SqsWorker extends AbstractWorker
 
         // In SQS, if an error occurs (exception for instance), the job is automatically reinserted
         // into the queue after a configured delay (the "visibility_timeout" option). If the job executed
-        // correctly, it must explicitely be removed
-        $job->execute();
-        $queue->delete($job);
+        // correctly, it must explicitly be removed
+        try {
+            $job->execute();
+            $queue->delete($job);
+        } catch (Exception $exception) {
+            // Do nothing, the job will be reinserted automatically for another try
+        }
     }
 }
